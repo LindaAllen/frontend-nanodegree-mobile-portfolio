c9ea24f64390611f9882c8590edf9d9d20ad6f78
Linda Allen <lindaallenx@gmail.com>
2016-11-29 18:15:00 -0500

    updated

--- views/js/main.js
+++ views/js/main.js
@@ -499,9 +499,9 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   // Optimized with transform3d and took some objects out from the loop
-  var phase = Math.sin(document.body.scrollTop / 1250 + (i%5));
 
   for (var i = 0; i < items.length; i++) {
+    var phase = Math.sin(document.body.scrollTop / 1250 + (i%5));
     items[i].style.transform = 'translate3d('+100 * phase +'px, 0px, 0px)';
   }
 
